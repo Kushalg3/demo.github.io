1b62e65b229f9aedf032e7df69c13ba55a897437
Kushalg3 <kushalghoshk@gmail.com>
2022-10-18 03:08:12 +0530

    creating get request for different pages

--- app.js
+++ app.js
@@ -2,9 +2,25 @@ const express = require("express");
 const app = express();
 
 app.get('/',(req,res) => {
-    res.send(`Hello world from the server.`);
-})
+    res.send("Hello world from the server.");
+});
+
+app.get('/about',(req,res) => {
+    res.send("This is about page.");
+});
+
+app.get('/contact',(req,res) => {
+    res.send("This is Contact us page.");
+});
+
+app.get('/signin',(req,res) => {
+    res.send("Signin here to continue.");
+});
+
+app.get('/signup',(req,res) => {
+    res.send("Register here to create a account.");
+});
 
 app.listen(3000,()=>{
     console.log("Server is listenning at port 3000.");
-})
+});
