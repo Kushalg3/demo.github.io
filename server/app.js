c55e69f14da8d9f7b2f955e7f7ef27fd61babf65
Kushalg3 <kushalghoshk@gmail.com>
2022-10-21 03:59:10 +0530

    using async await

--- server/app.js
+++ server/app.js
@@ -9,32 +9,33 @@ require('./db/conn');
 app.use(express.json());
 
 app.use(require('./router/auth'));
+const PORT = process.env.PORT;
 
-const middleware = (req,res,next) =>{
-    console.log("This is middleware.");
-    next();
-}
+// const middleware = (req,res,next) =>{
+//     console.log("This is middleware.");
+//     next();
+// }
 
-app.get('/',(req,res) => {
-    res.send("Hello world from the server.");
-});
+// app.get('/',(req,res) => {
+//     res.send("Hello world from the server.");
+// });
 
-app.get('/about',middleware,(req,res) => {
-    res.send("This is about page.");
-});
+// app.get('/about',middleware,(req,res) => {
+//     res.send("This is about page.");
+// });
 
-app.get('/contact',(req,res) => {
-    res.send("This is Contact us page.");
-});
+// app.get('/contact',(req,res) => {
+//     res.send("This is Contact us page.");
+// });
 
-app.get('/signin',(req,res) => {
-    res.send("Signin here to continue.");
-});
+// app.get('/signin',(req,res) => {
+//     res.send("Signin here to continue.");
+// });
 
-app.get('/signup',(req,res) => {
-    res.send("Register here to create a account.");
-});
+// app.get('/signup',(req,res) => {
+//     res.send("Register here to create a account.");
+// });
 
-app.listen(3000,()=>{
-    console.log("Server is listenning at port 3000.");
+app.listen(PORT,()=>{
+    console.log(`Server is listenning at ${PORT}.`);
 });
--- server/router/auth.js
+++ server/router/auth.js
@@ -1,4 +1,5 @@
 const express = require('express');
+require('../db/conn');
 const User = require('../model/userSchema');
 const router = express.Router();
 
@@ -6,22 +7,41 @@ router.get('/',(req,res) => {
     res.send("Hello world from the server from auth.js.");
 });
 
-router.post('/register',(req,res) => {
+// router.post('/register',(req,res) => {
+//     const {name, email, phone, work, password, cpassword} = req.body;
+//     if(!name || !email || !phone || !work || !password || !cpassword) {
+//         return res.status(422).json({error: "Enter all the required details carefully."});
+//     }
+
+//     User.findOne({email:email}).then(userExist => {
+//         if(userExist) {
+//             return res.status(422).json({error: "User already exist."});
+//         }
+
+//         const user = new User({name, email, phone, work, password, cpassword});
+//         user.save().then(() => {
+//             res.status(201).json({message:"Registration successful."});
+//         }).catch(err=> res.status(500).json({error: "Registration failed."}));
+//     }).catch(err => {console.log(err);});
+// });
+
+router.post('/register',async (req,res) => {
     const {name, email, phone, work, password, cpassword} = req.body;
     if(!name || !email || !phone || !work || !password || !cpassword) {
         return res.status(422).json({error: "Enter all the required details carefully."});
     }
 
-    User.findOne({email:email}).then(userExist => {
-        if(userExist) {
+    try {
+        const userExist = await User.findOne({email:email});
+        if (userExist) {
             return res.status(422).json({error: "User already exist."});
         }
-
         const user = new User({name, email, phone, work, password, cpassword});
-        user.save().then(() => {
-            res.status(201).json({message:"Registration successful."});
-        }).catch(err=> res.status(500).json({error: "Registration failed."}));
-    }).catch(err => {console.log(err);});
+        await user.save();
+        res.status(201).json({message:"Registration successful."});
+    } catch (error) {
+        console.log(error);
+    }
 });
 
 module.exports = router;
