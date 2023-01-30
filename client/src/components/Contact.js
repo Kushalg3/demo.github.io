fa0313505c95b35b30e03b42b87f551a7626a621
Kushal Ghosh <kushalghoshk@gmail.com>
2023-01-31 02:31:34 +0530

    rectifying mistakes in App.css

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -3,20 +3,28 @@ import phonepic from '../images/phone.png'
 
 const Contact = () => {
   return (
-    <div className='contact'>
+    <>
+    <div className='contact  d-flex justify-content-center align-content-center flex-wrap bg-white'>
       <div className='contact-info'>
-        <div className='container'>
-          <div className='row'>
-            <div className='col'>
+        <div className='Container'>
+          <div className='row justify-content-between'>
+            <div className='col-3 shadow bg-body-tertiary rounded p-3 bg-light '>
               <img src={phonepic} id='phone-img' className='me-1' alt='phone_image'/>
               <span>Phone</span>
             </div>
-            <div className='col'></div>
-            <div className='col'></div>
+            <div className='col-3 shadow bg-body-tertiary rounded p-3 bg-light'>
+              <img src={phonepic} id='phone-img' className='me-1' alt='phone_image'/>
+              <span>Phone</span>
+            </div>
+            <div className='col-3 shadow bg-body-tertiary rounded p-3 bg-light'>
+              <img src={phonepic} id='phone-img' className='me-1' alt='phone_image'/>
+              <span>Phone</span>
+            </div>
           </div>
         </div>
       </div>
     </div>
+    </>
   )
 }
 
