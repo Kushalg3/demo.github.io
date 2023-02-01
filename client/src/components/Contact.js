e19d29fb213d6b7e553d23787526617857b83274
Kushal Ghosh <kushalghoshk@gmail.com>
2023-02-01 19:05:22 +0530

    including images for contact page

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import phonepic from '../images/phone.png'
+import emailpic  from "../images/email.png"
+import locationpic from '../images/location.png'
 
 const Contact = () => {
   return (
@@ -8,17 +10,32 @@ const Contact = () => {
       <div className='Container'>
         <div className='contact-info'>
           <div className='row justify-content-between'>
-            <div className='col-3 box p-3 bg-light '>
-              <img src={phonepic} id='phone-img' className='me-1' alt='phone_image'/>
-              <span>Phone</span>
+            <div className='col-3 box px-2 py-3 bg-light row align-items-center'>
+              <div className='col-sm-3'>
+                <img src={phonepic} id='phone-img' className='me-1' alt='phone_image'/>
+              </div>
+              <div className='col-sm-9'>
+                <p className='contact-info-title'>Phone</p>
+                <p className='contact-info-text'>+916291294558</p>
+              </div>
             </div>
-            <div className='col-3 box p-3 bg-light'>
-              <img src={phonepic} id='phone-img' className='me-1' alt='phone_image'/>
-              <span>Phone</span>
+            <div className='col-3 box px-2 py-3 bg-light row align-items-center'>
+              <div className='col-sm-3'>
+                <img src={emailpic} id='phone-img' className='me-1' alt='phone_image'/>
+              </div>
+              <div className='col-sm-9'>
+                <p className='contact-info-title'>Email</p>
+                <p className='contact-info-text'>kushalghoshk@gmail.com</p>
+              </div>
             </div>
-            <div className='col-3 box p-3 bg-light'>
-              <img src={phonepic} id='phone-img' className='me-1' alt='phone_image'/>
-              <span>Phone</span>
+            <div className='col-3 box px-2 py-3 bg-light row align-items-center'>
+              <div className='col-sm-3'>
+                <img src={locationpic} id='phone-img' className='me-1' alt='phone_image'/>
+              </div>
+              <div className='col-sm-9'>
+                <p className='contact-info-title'>Address</p>
+                <p className='contact-info-text'>Kolkata, West Bengal, India</p>
+              </div>
             </div>
           </div>
         </div>
