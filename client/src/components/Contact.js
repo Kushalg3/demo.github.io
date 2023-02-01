a408a59496c6786350429e91cd8ae7ae73a50a65
Kushal Ghosh <kushalghoshk@gmail.com>
2023-02-01 23:21:16 +0530

    full contat page ui

--- client/src/components/Contact.js
+++ client/src/components/Contact.js
@@ -39,6 +39,29 @@ const Contact = () => {
             </div>
           </div>
         </div>
+        <div className='contact-form-container'>
+          <div className='contact-form box bg-light'>
+            <div className='container px-5 pt-3 pb-5'>
+              <div className='contact-form-title mb-4'>
+                <h1>Get In Touch</h1>
+              </div>
+              <div className='contact-details'>
+                <form className='contact-details-form'>
+                  <input type={'text'} name='name' id='name' placeholder='Your Name' autoComplete='off' required='true' />
+                  <input type={'email'} name='email' id='email' placeholder='Your email' autoComplete='off' required='true' />
+                  <input type={'tel'} name='phone' id='phone' placeholder='Your phone no.' autoComplete='off' required='true' />
+                </form>
+              </div>
+              <div className='message-field'>
+                <textarea id='message' placeholder='Write your problem....'></textarea>
+              </div>
+              <div className='submit'>
+                <button type='submit' className='btn btn-primary'>Send Message</button>
+              </div>
+            </div>
+          </div>
+        </div>
+        
       </div>
     </div>
     </>
