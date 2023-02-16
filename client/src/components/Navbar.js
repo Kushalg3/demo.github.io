426161721805bce28c54467778f3ad9bb5a0807c
Kushal Ghosh <kushalghoshk@gmail.com>
2023-02-16 20:50:13 +0530

    changed to not show about page when not login

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -59,11 +59,6 @@ const Navbar = () => {
                 </NavLink>
               </li>
               <li className="nav-item">
-                <NavLink className="nav-link" to={"/about"}>
-                  About
-                </NavLink>
-              </li>
-              <li className="nav-item">
                 <NavLink className="nav-link" to={"/contact"}>
                   Contact
                 </NavLink>
